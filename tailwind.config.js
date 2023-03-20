b2a99ff107e07dffe48ce8fe8d7ea65b405b44f0
mcmanyika <mcmanyika.com>
2023-03-20 13:52:29 -0500

    added tailwind elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./campuvista/**/*.{html,js}"],
+  content: [
+    "./campuvista/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"    
+],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 }
